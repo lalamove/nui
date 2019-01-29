730744f66600cf058f935a14ef5299c49ebe306e
Mikael Knutsson <mikael.knutsson@gmail.com>
2019-01-29 15:30:55 +0800

    Add tracing to Nui

---
+++ ntracing/ntracing.go
@@ -0,0 +1,24 @@
+package ntracing
+
+import (
+	"context"
+
+	opentracing "github.com/opentracing/opentracing-go"
+)
+
+type spanKey string
+
+// SpanKey is the key to use to store the span in request context
+var SpanKey = spanKey("span")
+
+// NewChildSpanFromContext Generates a child span with given name if a span is found on the context
+func NewChildSpanFromContext(ctx context.Context, name string) (opentracing.Span, bool) {
+	if span, ok := ctx.Value(SpanKey).(opentracing.Span); ok {
+		var childSpan = opentracing.StartSpan(
+			name,
+			opentracing.ChildOf(span.Context()),
+		)
+		return childSpan, true
+	}
+	return nil, false
+}
